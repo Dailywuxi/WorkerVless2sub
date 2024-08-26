6f5af8ba2d666b38118298d07b15e583da869582
Dailywuxi <116965363+Dailywuxi@users.noreply.github.com>
2024-08-27 02:41:51 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -326,7 +326,7 @@ export default {
 				uuid = env.UUID || "null";
 			}
 			
-			path = env.PATH || "/?proxyip=proxyip.aliyun.fxxk.dedyn.io";
+			path = env.PATH || "/?ed=2560";
 			sni = env.SNI || host;
 			type = env.TYPE || type;
 			edgetunnel = env.ED || edgetunnel;
